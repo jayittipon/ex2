29743d919e5df917daf48ca76022e56a3c7a7515
jayittipon <ittiponjackeaw@gmail.com>
2020-08-16 14:11:02 +0700

    uudate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $category = Post::all();
+        return $category;
+    }
+
+    public function show($id)
+    {
+        $category = Post::find($id);
+        return $category;
     }
 
     public function create()
